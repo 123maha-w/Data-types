bda6a1100fc6e20962d829bac866a124edbbff85
123maha-w <malikmahrush856@gmail.com>
2025-11-29 19:59:21 +0400

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,17 @@
+x=10
+
+a=5
+
+print ("type of a: ",type(a))
+
+b=2.5
+print ("type of b: ",type(b))
+
+c= "Mahrush Malik"
+print ("type of c: ",type(c))
+
+d= False
+print ("type of d: ",type(d))
+
+f= True
+print ("type: ",type(f))
